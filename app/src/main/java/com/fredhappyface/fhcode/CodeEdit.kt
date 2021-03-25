a45775004879abba91b9c8529954fb2468c996e0
Kieran BW <41634689+FredHappyface@users.noreply.github.com>
2021-03-25 19:13:25 +0000

    25/03/21_3
    more cleanup

--- app/src/main/java/com/fredhappyface/fhcode/CodeEdit.kt
+++
@@ -1,7 +0,0 @@
-package com.fredhappyface.fhcode
-
-import android.content.Context
-import android.util.AttributeSet
-import androidx.appcompat.widget.AppCompatEditText
-
-class CodeEdit(context: Context, attrs: AttributeSet, defStyleAttr: Int) : AppCompatEditText(context, attrs, defStyleAttr)
